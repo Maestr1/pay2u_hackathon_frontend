34bfac649805742aabc90ac01782a6adabaafb1f
Maestr1 <maestr234@gmail.com>
2024-03-18 09:31:47 +0300

    feat: добавляет стили бургера, подключает redux

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import {useState} from 'react';
+import './Header.scss'
 import NavBurger from './NavBurger/NavBurger.tsx';
+import BurgerButton from './BurgerButton/BurgerButton.tsx';
 
 function Header(props) {
+
+  const [navIsOpen, setNavIsOpen] = useState(false);
+
+  function handleClick() {
+    setNavIsOpen(!navIsOpen);
+  }
+
   return (
-    <header>
+    <header className='header'>
+      <BurgerButton onClick={handleClick} isMenuOpen={navIsOpen}/>
       <NavBurger/>
     </header>
   );
