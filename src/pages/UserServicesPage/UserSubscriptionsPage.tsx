ef55c4cf800305875a2417fdd260882b554f4846
maest <maestr234@gmail.com>
2024-04-04 19:06:00 +0300

    fix: коррекция стилей списка подписок пользователя

--- src/pages/UserServicesPage/UserSubscriptionsPage.tsx
+++ src/pages/UserServicesPage/UserSubscriptionsPage.tsx
@@ -5,6 +5,16 @@ import { Tab } from '@mui/material';
 import styled from 'styled-components';
 import UserCardList from '../../components/UserCardList/UserCardList';
 
+const Title = styled.h1`
+  margin: 0;
+  padding-block: 12px 20px;
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 1.5;
+  text-align: left;
+  color: var(--black);
+`;
+
 const CategoryImg = styled.img`
   max-width: 20px;
 `;
@@ -17,8 +27,6 @@ function UserSubscriptionsPage(): ReactElement {
   const categories = useSelectorTyped(
     (store) => store.servicesCategoriesReducer.servicesCategories
   );
-
-
 
   function handleTabChange(_event: React.SyntheticEvent, newValue: string) {
     setTabValue(newValue);
@@ -36,6 +44,7 @@ function UserSubscriptionsPage(): ReactElement {
 
   return (
     <section>
+      <Title>Мои подписки</Title>
       <TabContext value={tabValue}>
         <TabList
           onChange={handleTabChange}
