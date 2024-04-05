da0ee0099bf62aeea4ae6d6dccfc7fc408620127
Maestr1 <maestr234@gmail.com>
2024-04-05 19:36:32 +0300

    feat: добавлена кнопка "отменить" на форму поиска

--- src/components/SearchFrom/SearchFrom.tsx
+++ src/components/SearchFrom/SearchFrom.tsx
@@ -4,17 +4,20 @@ import searchIcon from '../../images/icons/search_icon.svg';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import api from '../../utils/api/Api';
 import _ from 'lodash';
+import { Button } from '@mui/material';
 
 interface IFormInput {
   query: string;
 }
 
 const SearchForm = styled.form`
+  position: relative;
   display: flex;
   justify-content: flex-start;
   padding-block: 8px;
 `;
 const Input = styled.input`
+  z-index: 10;
   box-sizing: border-box;
   padding-block: 6px;
   padding-right: 10px;
@@ -26,7 +29,7 @@ const Input = styled.input`
   font-family: InterVariable, sans-serif;
   font-size: 16px;
   line-height: 1.5;
-  caret-color: var(--navy-blue-1);
+  caret-color: var(--navy-blue-2);
   transition: width 0.3s ease-in-out;
 
   &::placeholder {
@@ -35,11 +38,22 @@ const Input = styled.input`
 
   &:focus-visible {
     outline-color: transparent;
-    width: 73%;
+    width: calc(100% - 6rem);
     &::placeholder {
       color: transparent;
     }
   }
+`;
+
+const ResetButton = styled.button`
+  position: absolute;
+  right: 0;
+  padding-block: 6px;
+  border: none;
+  background-color: transparent;
+  font-family: InterVariable, sans-serif;
+  font-size: 16px;
+  color: var(--navy-blue-2);
 `;
 
 function SearchFrom(): ReactElement {
@@ -55,17 +69,16 @@ function SearchFrom(): ReactElement {
     defaultValues: { query: '' },
   });
 
-
   useEffect(() => {
     // Создание дебаунс функции
     const debouncedSearch = _.debounce((query: string) => {
       api.searchServicesByQuery(query);
     }, 1000);
-  
+
     if (debouncedQuery) {
       debouncedSearch(debouncedQuery);
     }
-  
+
     // Очистка дебаунс функции, чтобы предотвратить утечки памяти
     return () => {
       debouncedSearch.cancel();
@@ -92,6 +105,7 @@ function SearchFrom(): ReactElement {
         type="text"
         placeholder="Кино, музыка и другие сервисы"
       />
+      <ResetButton onClick={() => reset()} type="reset">Отменить</ResetButton>
     </SearchForm>
   );
 }
